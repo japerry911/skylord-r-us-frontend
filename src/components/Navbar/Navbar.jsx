f23d900b645a2f1b2901fad94598c1aca2117f5a
Jack Perry <jack.e.perry@outlook.com>
2021-12-27 21:19:29 -0700

    👌 IMPROVE: nav bar set up, no functionality yet, just css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Navbar.css';
+import TransparentLogo250 from '../../assets/logo-transparent-250.png';
 
 const Navbar = () => {
   return (
     <div className="nav-main-div">
-      <h1>Nav Bar insert here</h1>
+      <div className="logo-div">
+        <img
+          src={TransparentLogo250}
+          alt="logo transparent background"
+          className="logo-img"
+        />
+      </div>
+      <div className="nav-links-div">
+        <a className="link-btn">Home</a>
+        <a className="link-btn">About</a>
+        <a className="link-btn">Shop</a>
+        <a className="link-btn">Contact</a>
+      </div>
+      <div className="icons-right-div">
+        <i className="fas fa-user-alt icons"></i>
+        <i className="fas fa-shopping-cart icons"></i>
+      </div>
     </div>
   );
 };
