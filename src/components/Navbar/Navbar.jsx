558b31435813ba5273d2262b6842945eb618ca8a
Jack Perry <jack.e.perry@outlook.com>
2021-12-27 19:37:03 -0700

    📦 NEW: foundation set

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="nav-main-div">
+      <h1>Nav Bar insert here</h1>
+    </div>
+  );
+};
+
+export default Navbar;
